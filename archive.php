120c6d07ad88c35f017b2a3979e4a623c9f78e00
haikararou <chimiriho7@yahoo.co.jp>
2023-03-27 11:57:30 +0900

    20230327-001

--- archive.php
+++ archive.php
@@ -13,77 +13,103 @@ jQuery(function(){
 });
 </script>
 </head>
-<body id="sub" class="report topics summer">
+<body id="sub" class="report winter">
 <div id="container">
 <?php get_template_part( 'nav' ); ?>
-		<!-- /#header -->
-		<!--<div id="topArea">
-		<?php global $post;
-		$args = array(
-		'post_type' => 'post',
-		'term' => 'headerimg',
-		'numberposts' => 1
-		);
-		$myposts = get_posts( $args );
-		foreach( $myposts as $post ) :  setup_postdata($post);?>
-		<p align="center" class="topicsHead"><?php echo get_custom_img('news_header','stay_top'); ?></p>
-		<?php endforeach; ?>
-		</div>-->
-		<?php wp_reset_query() ?>
-		<div id="pageTitle">
-		<h2>最新情報アーカイブ</h2>
+	<!-- /#header -->
+	<div id="reportBox">
+	<?php if(is_date()): ?><!--月別アーカイブの場合-->
+	<!-- <ul id="tab" class="clearfix">
+		<li class="tab01"><a href="#reportBox" class="select"><span>ゲレンデレポート</span><i class="fa fa-caret-down"></i></a></li>
+		<li class="tab02"><a href="<?php echo home_url();?>/trekkingreport/"><span>トレッキングレポ（白馬）</span><i class="fa fa-caret-down"></i></a></li>
+	</ul> -->
+	<!-- <div id="pageTitle" class="pageTitle2"><h2>ゲレンデレポート月別アーカイブ一覧</h2></div>
+	<div class="content_wrap2"><h3><?php echo get_post_time( 'Y年m月' ); ?>の記事</h3></div> -->
+	<div class="content_wrap gereBox">
+		<div class="enBox">
+			<ul class="infini">
+				<?php if(have_posts()):while(have_posts()):the_post(); ?>
+				<li class="post">
+					<a href="<?php the_permalink(); ?>">
+						<p class="tag"><span><?php $terms = wp_get_object_terms($post->ID,'report'); foreach($terms as $term){echo $term->name . ' ';} ?></span></p>
+						<p class="photo"><?php echo get_custom_img('ski_img','ski_s'); ?></p>
+						<p class="time"><?php echo get_post_time( 'Y.m.d' ); ?></p>
+						<h5><?php the_title(); ?></h5>
+					</a>
+				</li>
+				<?php endwhile;endif; ?>
+				<?php wp_reset_postdata(); ?>
+			</ul>
 		</div>
-		<div id="punkuz">
-		<a href="../">HOME</a> > <a href="<?php echo home_url();?>/category/news/">最新情報</a> > <?php echo get_post_time( 'Y年m月' ); ?>
+		<div class="pagenation font">
+			<?php global $wp_rewrite;
+			$paginate_base = get_pagenum_link(1);
+			if(strpos($paginate_base, '?') || ! $wp_rewrite->using_permalinks()){
+				$paginate_format = '';
+				$paginate_base = add_query_arg('paged','%#%');
+			}
+			else{
+				$paginate_format = (substr($paginate_base,-1,1) == '/' ? '' : '/') .
+				user_trailingslashit('page/%#%/','paged');;
+				$paginate_base .= '%_%';
+			}
+			echo paginate_links(array(
+				'base' => $paginate_base,
+				'format' => $paginate_format,
+				'total' => $wp_query->max_num_pages,
+				'mid_size' => 4,
+				'current' => ($paged ? $paged : 1),
+				'prev_text' => '«',
+				'next_text' => '»',
+			)); ?>
 		</div>
-		<div id="content">
-		<div class="main">
-		<div class="entory">
-		<h3><?php echo get_post_time( 'Y年m月' ); ?>の記事</h3>
-		<ul class="month">
-		<?php if(have_posts()):while(have_posts()):the_post(); ?>
-			<li>
-				<a href="<?php the_permalink(); ?>">
-				<p class="day"><span><?php echo get_post_time( 'd' ); ?></span><?php echo get_post_time( 'm.Y' ); ?></p>
-				<h5><?php the_title(); ?> </h5>
-				</a>
-			</li>
-		<?php endwhile;endif; ?>
-		</ul>
-		
-		
+	</div>
+	<?php else : ?><!-- アーカイブのとき -->
+	<!-- <ul id="tab">
+		<li class="tab01"><a href="#reportBox" class="select"><span>ゲレンデレポート</span><i class="fa fa-caret-down"></i></a></li>
+		<li class="tab02"><a href="<?php echo home_url();?>/trekkingreport/"><span>トレッキングレポ（白馬）</span><i class="fa fa-caret-down"></i></a></li>
+	</ul> -->
+	<div class="content_wrap2"><h3><?php the_archive_title( $before, $after ); ?>の記事</h3></div>
+	<div class="content_wrap gereBox">
+		<div class="enBox">
+			<ul class="infini">
+				<?php if(have_posts()):while(have_posts()):the_post(); ?>
+				<li class="post">
+					<a href="<?php the_permalink(); ?>">
+						<p class="tag"><span><?php $terms = wp_get_object_terms($post->ID,'report'); foreach($terms as $term){echo $term->name . ' ';} ?></span></p>
+						<p class="photo"><?php echo get_custom_img('ski_img','ski_s'); ?></p>
+						<p class="time"><?php echo get_post_time( 'Y.m.d' ); ?></p>
+						<h5><?php the_title(); ?></h5>
+					</a>
+				</li>
+				<?php endwhile;endif; ?>
+				<?php wp_reset_postdata(); ?>
+			</ul>
 		</div>
-<!-- pager -->
-<div class="pagenate font">
-	<?php global $wp_rewrite;
-	$paginate_base = get_pagenum_link(1);
-	if(strpos($paginate_base, '?') || ! $wp_rewrite->using_permalinks()){
-		$paginate_format = '';
-		$paginate_base = add_query_arg('paged','%#%');
-	}
-	else{
-		$paginate_format = (substr($paginate_base,-1,1) == '/' ? '' : '/') .
-		user_trailingslashit('page/%#%/','paged');;
-		$paginate_base .= '%_%';
-	}
-	echo paginate_links(array(
-		'base' => $paginate_base,
-		'format' => $paginate_format,
-		'total' => $wp_query->max_num_pages,
-		'mid_size' => 2,
-		'current' => ($paged ? $paged : 1),
-		'prev_text' => '<i class="fa fa-angle-double-left"></i> PREV',
-		'next_text' => 'NEXT <i class="fa fa-angle-double-right"></i>',
-	)); ?>
-</div>	
-<!-- pager -->
-
-
-<!-- /pager	 -->		
+		<div class="pagenation font">
+			<?php global $wp_rewrite;
+			$paginate_base = get_pagenum_link(1);
+			if(strpos($paginate_base, '?') || ! $wp_rewrite->using_permalinks()){
+				$paginate_format = '';
+				$paginate_base = add_query_arg('paged','%#%');
+			}
+			else{
+				$paginate_format = (substr($paginate_base,-1,1) == '/' ? '' : '/') .
+				user_trailingslashit('page/%#%/','paged');;
+				$paginate_base .= '%_%';
+			}
+			echo paginate_links(array(
+				'base' => $paginate_base,
+				'format' => $paginate_format,
+				'total' => $wp_query->max_num_pages,
+				'mid_size' => 4,
+				'current' => ($paged ? $paged : 1),
+				'prev_text' => '«',
+				'next_text' => '»',
+			)); ?>
 		</div>
-<?php get_sidebar("topics"); ?>
-
-
-		</div>
-		<!-- /#content -->
+	</div>
+	<?php endif; ?>
+</div>
+<!--REPORTここまで-->
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -441,3 +441,28 @@ function get_cpt_calendar($cpt, $initial = true, $echo = true) {
         return apply_filters( 'get_calendar',  $calendar_output );
 
 }
+
+
+
+
+/* the_archive_title 余計な文字を削除 */
+add_filter( 'get_the_archive_title', function ($title) {
+    if (is_category()) {
+        $title = single_cat_title('',false);
+    } elseif (is_tag()) {
+        $title = single_tag_title('',false);
+	} elseif (is_tax()) {
+	    $title = single_term_title('',false);
+	} elseif (is_post_type_archive() ){
+		$title = post_type_archive_title('',false);
+	} elseif (is_date()) {
+	    $title = get_the_time('Y年n月');
+	} elseif (is_search()) {
+	    $title = '検索結果：'.esc_html( get_search_query(false) );
+	} elseif (is_404()) {
+	    $title = '「404」ページが見つかりません';
+	} else {
+
+	}
+    return $title;
+});
